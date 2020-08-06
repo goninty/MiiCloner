7e4a8182bac3ad91daed1b111fac445becfc4db5
Andrew Morton <andrewmorton2@hotmail.co.uk>
2020-08-06 20:37:22 +0100

    Updated publisher info.

--- MiiCloner/Properties/AssemblyInfo.cs
+++ MiiCloner/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MiiCloner")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A tool to clone Mii characters.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("goninty")]
 [assembly: AssemblyProduct("MiiCloner")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
